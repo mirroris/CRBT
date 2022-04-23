45bde3a8faefad736be5346856a33e745bc3bd70
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-04-23 11:55:12 +0100

    resist

--- foreign.js
+++ foreign.js
@@ -2,6 +2,9 @@ function oneofFour(num){
     let idname = "question"+num;
     console.log(idname);
     var question = document.getElementById(idname);
-  question.style.display = "none";
-  return;
+    question.style.display = "none";
+    let nextidname = "question"+(num++);
+    var nextquestion = document.getElementById(nextidname);
+    nextquestion.style.display = ""
+    return num;
 }

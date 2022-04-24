e9ecaa51f9592b2adf637be41d2478137061c1a1
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-04-24 06:23:56 +0100

    create oneq-oneans system

--- foreign.js
+++ foreign.js
@@ -1,11 +1,11 @@
 function oneofFour(){
     let idname = "question"+num;
     console.log(idname);
-    var question = document.getElementById(idname);
+    let question = document.getElementById(idname);
     question.style.display = "none";
     let nextidname = "question"+(++num);
     console.log(nextidname);
-    var nextquestion = document.getElementById(nextidname);
+    let nextquestion = document.getElementById(nextidname);
     nextquestion.style.display = "block";
     return num;
 }

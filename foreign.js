05c03902f0343a6a353febe458f1f9c966bbb53f
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-04-24 08:56:18 +0100

    create backbutton

--- foreign.js
+++ foreign.js
@@ -1,11 +1,29 @@
 function oneofFour(){
     let idname = "question"+num;
-    console.log(idname);
+    //console.log(idname);
     let question = document.getElementById(idname);
     question.style.display = "none";
     let nextidname = "question"+(++num);
-    console.log(nextidname);
+    //console.log(nextidname);
     let nextquestion = document.getElementById(nextidname);
     nextquestion.style.display = "block";
+    
+    /**register answer info (answer (n))*/
+    
+    return num;
+}
+
+function back(){
+    let idname = "question"+num;
+    //console.log(idname);
+    let question = document.getElementById(idname);
+    question.style.display = "none";
+    let nextidname = "question"+(--num);
+    //console.log(nextidname);
+    let nextquestion = document.getElementById(nextidname);
+    nextquestion.style.display = "block";
+    
+    /* delete answer info[answer (--num)] */
+
     return num;
 }

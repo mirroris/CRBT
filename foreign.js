b02a7d73ce896ac0b9f3fdc7efdf780f9e7ee3d1
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-04-30 17:03:08 +0100

    f

--- foreign.js
+++ foreign.js
@@ -1,23 +1,24 @@
 
-var valueofAnswerTab =[];
-var answer1_1 = [];
-var answer1_2 = [];
-var answer1_3 = [];
-var answer1_4 = [];
-var answer2_1 = [];
-var answer2_2 = [];
-var answer2_3 = [];
-var answer3_1 = [];
-var answer3_2 = [];
-var answer3_3 = [];
-var answer3_4 = [];
-var answer4_1 = [];
-var answer4_2 = [];
-var answer4_3 = [];
-var answer5_1 = [];
-var answer5_2 = [];
-var answer5_3 = [];
-var answer5_4 = [];
+var valueofAnswerTab =[5][4]=[
+    [[],   //
+    [],   
+    [],  
+    []], 
+    [],   //
+    [],
+    [],
+    [],   //
+    [],    
+    [],   
+    [],   
+    [],  //
+    [],
+    [], 
+    [], //
+    [],
+    [],
+    [],
+];
 
 var score = {typeA:0,typeB:0,typeC:0,typeD:0,typeE:0,typeF:0,typeG:0,typeH:0};
 var chart=[];
@@ -56,6 +57,7 @@ function back(){
 }
 
 function accumulate(){
+
     //どの性格かを求める
     console.log(chart);
     //性格に合わせたデータを取得
@@ -65,5 +67,6 @@ function accumulate(){
     let result = document.getElementById("resultsheet");
     //リザルトを表示
     result.style.display = "block";
+    document.getElementsByClassName("topbar").style.display="none";
     return;
 }

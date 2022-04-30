eab72f02217542d2adba976cdf13b4244510e7eb
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-04-30 19:51:07 +0100

    hi

--- foreign.js
+++ foreign.js
@@ -4,23 +4,23 @@ var valueofAnswerTab ={
     12:[10.1,10.1,10.1,2.1,2.1,2.1,5.1,2.1],
     13:[5.1,2.1,5.1,2.1,2.1,10.1,10.1,5.1],
     14:[2.1,2.1,2.1,10.1,5.1,5.1,2.1,10.1],
-    21:[],
-    22:[],
-    23:[],
-    31:[],
-    32:[],
-    33:[],
-    34:[],
-    41:[],
-    42:[],
-    43:[],
-    51:[],
-    52:[],
-    53:[],
-    54:[]
+    21:[0.001,3.001,0.001,0.001,3.001,0.001,0.001,0.001],
+    22:[0.001,0.001,3.001,3.001,0.001,3.001,0.001,0.001],
+    23:[3.001,0.001,0.001,0.001,0.001,0.001,3.001,3.001],
+    31:[5.0001,5.0001,10.0001,5.0001,2.0001,2.0001,2.0001,10.0001],
+    32:[2.0001,10.0001,2.0001,2.0001,5.0001,10.0001,2.0001,2.0001],
+    33:[10.0001,2.0001,5.0001,2.0001,2.0001,2.0001,10.0001,5.0001],
+    34:[2.0001,2.0001,2.0001,10.0001,10.0001,5.0001,5.0001,2.0001],
+    41:[2.01,2.01,2.01,0.01,2.01,0.01,0.01,2.01],
+    42:[1.01,1.01,1.01,1.01,1.01,1.01,1.01,1.01],
+    43:[0.01,0.01,0.01,2.01,0.01,2.01,2.01,0.01],
+    51:[10,10,10,5,5,2,2,2],
+    52:[2,2,2,2,2,5,5,10],
+    53:[2,2,2,2,2,10,10,5],
+    54:[5,5,5,10,10,2,2,2]
 };
 
-var score = {typeA:0,typeB:0,typeC:0,typeD:0,typeE:0,typeF:0,typeG:0,typeH:0};
+var score = [0,0,0,0,0,0,0,0];
 var chart=[];
 
 
@@ -37,6 +37,7 @@ function oneofFour(key){
     /**register answer info (answer (n))*/
     //chart.push();
     console.log(key);
+    chart.push(key);
     console.log(valueofAnswerTab[key])
     return num;
 }
@@ -59,8 +60,51 @@ function back(){
 
 function accumulate(){
 
+    let chtype;
+
     //どの性格かを求める
     console.log(chart);
+    for(let i=0; i<5;i++){
+        let x = valueofAnswerTab[chart[i]];
+        score = score + x;
+        console.log(score);
+    }
+
+    for(let i=0; i<8; i++){
+        let max = 0;
+        if(max<=score[i]){
+            max=score[i];
+            chtype = i;
+        }
+    }
+
+    let Type={};
+    switch(i+1){
+        case 1:
+            Type.color="E83419"; //テーマカラ-
+            Type.info='<div id = "critical">フ</div>ットワークが軽く、何事にも意欲的で周囲の人を引っ張っていくリーダー的な性格です。<br>実はアーティスティックな一面も。<br>身体を動かすことが好きで、動きやすくカジュアルなファッションがお似合いです！<br>ファッションに柄のマイバックを取り入れる事で、より一層あなたの魅力が光ります✨<br>';   // 基本情報
+            Type.item='マイバッグ';  // アイテム
+            Type.img='result1.png';   // キャラ画像
+            Type.beans=''; //豆知識
+            Type.beansSub='【豆知識】本当にエコバックってエコなの？？';
+            break;
+        case 2:
+            //ここに書く
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+    }
+
     //性格に合わせたデータを取得
 
     //リザルトに設定
@@ -68,6 +112,16 @@ function accumulate(){
     let result = document.getElementById("resultsheet");
     //リザルトを表示
     result.style.display = "block";
-    document.getElementByClassId("topbar").style.display="none";
+    result.style.backgroundColor=Type.color;
+
+    //動的に結果を作成
+    let resultChara = '<div class = "character">'+'<img src='+Type.img+' class="resize">';
+    let resultintro = resultChara+"</div><br>"+"<div id = 'bold'>あなたにおすすめのエコアイテムは<br>「"+Type.item+"」<br>"
+    
+    let info = '<div class = "iteminfo" id = "iteminfo"><div id="normal">'+Type.info+"</div></div>";
+    
+    let beans =  '<div class = "beans"> <div id = "bold">'+Type.beansSub+'</div>'+SVGUnitTypes.beans+'</div>';
+
+    document.getElementById("resultsheet").innerHTML=resultintro+info+beans;
     return;
 }

ef26ffb9c631f15fd6d08376da0b8a6c28a648b3
Tsuka <hana1319otk@gmail.com>
2022-05-02 00:29:52 +0900

    結果画面

--- foreign.js
+++ foreign.js
@@ -166,7 +166,7 @@ function accumulate(){
    
     let info = '<div class = "iteminfo" id = "iteminfo"><div id="normal">'+Type.info+"</div></div>";
    
-    let beans =  '<div class = "beans"> <div id = "bold">'+Type.beansSub+'</div>'+Type.beans+'</div>';
+    let beans =  '<div class = "beans"> <div id = "bold">'+Type.beans+'</div>'+Type.beansSub+'</div>';
     let twinfo = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-text="【阪大生ファッション×エコアイテム診断】あなたのエコアイテムは…' +Type.item+ '…！' +Type.item+ 'を持って阪大へ行こう！ #阪大生診断"> Tweet</a>';
     document.getElementById("resultsheet").innerHTML=resultintro+info+beans+twinfo;
     //color調整

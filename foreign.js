d60c54171b110960872f7ff0c5680e92e2fe465e
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-05-03 03:16:41 +0100

    textCOntentを追加

--- foreign.js
+++ foreign.js
@@ -156,7 +156,7 @@ function accumulate(){
     //shareボタンの可視化
     let twdata = document.getElementById("hault");
     twdata.style.opacity=1;
-    twdata.dataText = "あなたもキャリボト診断どうですか？？";
+    twdata.textContent = "あなたもキャリボト診断どうですか？？";
 
     //リザルトに設定
     document.getElementById("question6").style.display="none";

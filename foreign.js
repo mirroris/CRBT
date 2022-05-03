f3a13194ebfc8d04f74fbb8bab023992bbbd906d
Fuma Suenaga <u455017k@ecs.osaka-u.ac.jp>
2022-05-03 03:13:11 +0100

    dataTextの追加

--- foreign.js
+++ foreign.js
@@ -154,9 +154,9 @@ function accumulate(){
     //性格に合わせたデータを取得
 
     //shareボタンの可視化
-    
     let twdata = document.getElementById("hault");
     twdata.style.opacity=1;
+    twdata.dataText = "あなたもキャリボト診断どうですか？？";
 
     //リザルトに設定
     document.getElementById("question6").style.display="none";
